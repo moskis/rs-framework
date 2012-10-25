c2f00cee28d5975cadcdf9f88ccb359192d0dab9
Jose Pardilla <info@josepardilla.com>
2012-10-25 12:09:19 +0200

    add Options Framework by Devin Price

--- init.php
+++ init.php
@@ -7,9 +7,6 @@
 // RS functions file
 require_once("functions.php");
 
-// Options Framework
-//require_once("options-framework.php");
-
 // Advanced Body & Post classes for styling
 require_once("inc/post-classes.php");
 
@@ -45,3 +42,15 @@ require_once("inc/post-classes.php");
 
 // Extra fields in user profiles
 //require_once("inc/user-meta.php");
+
+
+/*
+ * Options Framework
+ */
+
+require_once( 'options-framework/options.php' );
+
+if ( !function_exists( 'optionsframework_init' ) ) {
+	define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/rs-framework/options-framework/inc/' );
+	require_once dirname( __FILE__ ) . '/options-framework/inc/options-framework.php';
+}
